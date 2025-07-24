1ac3f96ec561d9085eb9069ac6d9c77dd7f1e89b
vichhu08 <vichhuviswanathan@gmail.com>
2025-07-24 21:23:13 +0530

    answer submitted

--- 04_mathEquations/mathEquations.js
+++ 04_mathEquations/mathEquations.js
@@ -1,9 +1,15 @@
 const a = 1 - 1 // Freebie!!! This is the answer to "one minus one"
-const b = "one plus eight"
-const c = "22 times three"
-const d = "the *remainder* of 5/4"
-const e = "the variable 'b' minus 17"
-const f = "the sum of the previous five variables"
+b = "one plus eight"
+c = "22 times three"
+d = "the *remainder* of 5/4"
+e = "the variable 'b' minus 17"
+f = "the sum of the previous five variables"
+
+b = 1+8;
+c = 22 * 3;
+d = 5%4;
+e = b - 17 ;
+f = a+b+c+d+e;
 
 // Do not edit below this line
 module.exports = {a, b, c, d, e, f}
